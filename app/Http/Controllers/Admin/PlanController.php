e67f2a298ec3c36bbb23c0204ffa1d1febf99d45
Eduardo WB <eduardo@agenciacrow.com.py>
2021-08-25 11:36:26 -0300

    list all plans

--- app/Http/Controllers/Admin/PlanController.php
+++ app/Http/Controllers/Admin/PlanController.php
@@ -2,13 +2,25 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Plan;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class PlanController extends Controller
 {
+    private $repository;
+
+    public function __construct(Plan $plan)
+    {
+        $this->repository = $plan;
+    }
+
     public function index()
     {
-        return view('admin.pages.plans.index');
+        $plans = $this->repository->all();
+
+        return view('admin.pages.plans.index', [
+            'plans' => $plans,
+        ]);
     }
 }
--- resources/views/admin/pages/plans/index.blade.php
+++ resources/views/admin/pages/plans/index.blade.php
@@ -3,9 +3,39 @@
 @section('title', 'Planos')
 
 @section('content_header')
-    <h1>Planos</h1>
+<h1>Planos</h1>
 @stop
 
 @section('content')
-    <p>Listagem dos planos</p>
+<div class="card">
+    <div class="card-header">
+        #filtros
+    </div>
+    <div class="card-body">
+        <table class="table table-condensed">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Preço</th>
+                    <th width="50">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($plans as $plan)
+                <tr>
+                    <td>
+                        {{ $plan->name }}
+                    </td>
+                    <td>
+                        {{ $plan->price }}
+                    </td>
+                    <td style="width: 10px;">
+                        <a href="#" class="btn btn-warning">VER</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 @stop
