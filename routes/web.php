b8334f97d77791da95260c16f2af8e6ce6f0c792
Eduardo WB <eduardo@agenciacrow.com.py>
2021-09-01 10:43:59 -0300

    setup site initial page and layout

--- routes/web.php
+++ routes/web.php
@@ -65,9 +65,7 @@ Route::prefix('admin')
         Route::get('/', 'PlanController@index')->name('admin.index');
     });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Site\SiteController@index')->name('site.home');
 
 /**
  * Auth Routes
