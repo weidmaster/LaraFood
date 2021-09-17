d2c432931bc5695b9a9b7eb0f2a433f3696a2cdc
Eduardo WB <eduardo@agenciacrow.com.py>
2021-09-17 10:20:18 -0300

    add pagination on tenants list api

--- app/Http/Controllers/Api/TenantApiController.php
+++ app/Http/Controllers/Api/TenantApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Services\TenantService;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\TenantResource;
+use Illuminate\Http\Request;
 
 class TenantApiController extends Controller
 {
@@ -15,14 +16,19 @@ class TenantApiController extends Controller
         $this->tenantService = $tenantService;
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        return TenantResource::collection($this->tenantService->getAllTenants());
+        $per_page = (int) $request->get('per_page', 15);
+        $tenants = $this->tenantService->getAllTenants($per_page);
+
+        return TenantResource::collection($tenants);
     }
 
     public function show($uuid)
     {
-        $tenant = $this->tenantService->getTenantByUuid($uuid);
+        if (!$tenant = $this->tenantService->getTenantByUuid($uuid)) {
+            return response()->json(['message' => 'Not Found'], 404);
+        }
 
         return new TenantResource($tenant);
     }
