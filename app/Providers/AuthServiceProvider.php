59cac9f056c9503ba7c8f53d663d312aed421b62
Eduardo WB <eduardo@agenciacrow.com.py>
2021-09-23 14:33:47 -0300

    prepare laravel for testing

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,6 +27,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
+        if ($this->app->runningInConsole()) {
+            return;
+        }
+
         $permissions = Permission::all();
 
         foreach ($permissions as $permission) {
