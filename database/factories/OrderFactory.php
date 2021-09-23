416c018d09ed79c6c22ccc9547f5a09f19c42ea8
Eduardo WB <eduardo@agenciacrow.com.py>
2021-09-23 16:24:32 -0300

    create factories for testing

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Order;
+use App\Models\Tenant;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+$factory->define(Order::class, function (Faker $faker) {
+    return [
+        'tenant_id' => factory(Tenant::class),
+        'identify' => uniqid() . Str::random(10),
+        'total' => 80.0,
+        'status' => 'open'
+    ];
+});
