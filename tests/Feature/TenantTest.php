baa957fae42b512bce1e8ee2273611db5c477092
Eduardo WB <eduardo@agenciacrow.com.py>
2021-09-24 10:50:46 -0300

    test tenants api

---
+++ tests/Feature/TenantTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Tenant;
+use Tests\TestCase;
+
+class TenantTest extends TestCase
+{
+    /**
+     * Test get all Tenants.
+     *
+     * @return void
+     */
+    public function testGetAllTenants()
+    {
+        factory(Tenant::class, 10)->create();
+
+        $response = $this->get('/api/v1/tenants');
+
+        $response->assertStatus(200)
+            ->assertJsonCount(10, 'data');
+    }
+
+    /**
+     * Test get error single Tenant
+     *
+     * @return void
+     */
+    public function testErrorGetTenant()
+    {
+        $tenant = 'fake_value';
+
+        $response = $this->get("/api/v1/tenants/{$tenant}");
+
+        $response->assertStatus(404);
+    }
+
+    /**
+     * Test get single Tenant
+     *
+     * @return void
+     */
+    public function testGetTenantByIdentify()
+    {
+        $tenant = factory(Tenant::class)->create();
+
+        $response = $this->get("/api/v1/tenants/{$tenant->uuid}");
+
+        $response->assertStatus(200);
+    }
+}
